75d4ce554fcab103b67c6cac86f91094d6bcd7ba
Monica147 <monica14mdu@gmail.com>
2023-10-13 23:35:33 +0530

    resume

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementById("h1").innerHTML="MONICA R";
+document.getElementById("h4").innerHTML="ASPIRING FULL STACK DEVELOPER";
+document.getElementById("inforMation").innerHTML="Information<hr/>";
+document.getElementById("information").innerHTML="Madurai 7904110142 monica14mdu@gmail.com";
+document.getElementById("h2").innerHTML="EDUCATION <hr/>";
+document.getElementById("paraContent").innerHTML=" NPR college of Engineering and technology <br/>BE.CSE <br/>89%";
+document.getElementById("h2_1").innerHTML="Career Objective <hr/>";
+document.getElementById("careerObj").innerHTML="Aspiring Full Stack Developer exhibiting a foundation in full stack with skill of programming to deliver an immense and engaging user experience through efficient web development";
+document.getElementById("h2_2").innerHTML="Project Title <hr/>";
+document.getElementById("projectTitle").innerHTML="Graphical Password Authenticator<br/>The frontend was developed with HTML and CSS For the backend Google cloud Database was used.";
+document.getElementById("skills").innerHTML=("<tr><td>HTML</td><td>Articulate</td></tr><tr><td>CSS</td><td>Versatile</td></tr><tr><td>Javascript</td><td>Managing skills</td></tr><tr><td>MY sql</td><td>Mentoring skill</td></tr>");
+document.getElementById("h2_3").innerHTML="Certications<hr/>";
+document.getElementById("CertiFications").innerHTML=("Conference paper - Chatbot on Solaimalai Engineering <br/>Conference Paper Virtual Purchasing System Using Vending Machine and QR Code");
+document.getElementById("h2_4").innerHTML="Internship<hr/>";
+document.getElementById("internShip").innerHTML="Internship at Oasis Infobyte : Web Development <br./>KGiSL Microcollege Coimbatore";
+document.getElementById("h2_5").innerHTML="Languages Known<hr/>";
+
+
+
+
+
+
